cab44bc6f2d28dae19f3a4e7b174003f7c329628
Osahon <horsirhon@gmail.com>
2022-08-24 21:23:37 +0100

    no Bootstrap version with jquery and css variables

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
 $(document).ready(function(){
     $("footer a").mouseover(function(){
-        $(this).css("border-bottom", "solid 2px white").fadeIn(200);
+        $(this).css("border-bottom", "solid 2px white");
     });
     $("footer a").mouseleave(function(){
         $(this).css("border-bottom", "solid 2px transparent");
     });
+    $("header li a").mouseover(function(){
+        $(this).css("border-bottom", "solid 2px white");
+        $(this).css("opacity", 1);
+    });
+    $("header li a").mouseleave(function(){
+        $(this).css("border-bottom", "solid 2px transparent");
+        $(this).css("opacity", .9);
+    });
+    $("#footer-icons div").mouseover(function(){
+        $(this).css("border-bottom", "solid 2px white");
+    });
+    $("#footer-icons div").mouseleave(function(){
+        $(this).css("border-bottom", "solid 2px transparent");
+    });
+    $("header #ham").click(function(){
+        $("header ul").toggle();
+    });
+    
+    if(stripUnit($('body').css('width')) <= 1300){
+        $('header ul').hide();
+        $('header ul').click(function(){
+            $('header ul').hide();
+        })
+    }
+    if(stripUnit($('body').css('width')) >= 1300){
+        $('header ul').show();
+    }
+
+
+
+
+    function stripUnit(num){
+        num = num.split('');
+        num.pop();
+        num.pop();
+        return Number(num.join(''));
+    }
+    if(stripUnit($('body').css('width')) >= 1300){
+        $('header ul').show();
+    }
 });
